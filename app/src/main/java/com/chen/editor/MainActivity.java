65818960fd88a156a026c6f2b3de499f74886fc1
chenjc <1911689973@qq.com>
2019-02-19 16:20:42 +0800

    为图片编辑控件设置图片。

--- app/src/main/java/com/chen/editor/MainActivity.java
+++ app/src/main/java/com/chen/editor/MainActivity.java
@@ -1,15 +1,48 @@
 package com.chen.editor;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import com.chen.editor.widget.EditImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity {
+
+    private EditImageView mEditImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initEditView();
+
+
+    }
+
+    private Bitmap getBitmap() {
+        try {
+            InputStream flowerStream = getResources().getAssets().open("flower.jpg");
+            Bitmap bitmap = BitmapFactory.decodeStream(flowerStream);
+
+
+            return bitmap;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void initEditView() {
+        mEditImageView = (EditImageView) findViewById(R.id.activity_main_edit_image);
+        Bitmap bitmap = getBitmap();
+        mEditImageView.setImage(bitmap);
 
     }
 }
